0d211e13cd65411f921f2924687870eefe1ca8c5
klee8880 <klee8880@gmail.com>
2020-04-07 00:45:46 -0500

    Added Server Side

---
+++ src/Server/InetServer.java
@@ -0,0 +1,28 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+
+public class InetServer {
+
+	public static void main (String [] args) throws IOException {
+		int q_len = 6;
+		int port = 1565;
+		Socket sock;
+		
+		//Create a socket that listens in on a specified port.
+		ServerSocket servsock = new ServerSocket (port, q_len);
+		
+		System.out.println("ClarkElliot's Inet server 1.8 starting up, listeing at port 1565./n");
+		
+		//Busy waiting for a new connection request.
+		while (true) {
+			//Initialize a new connection
+			sock = servsock.accept();
+			//Run program with the connection.
+			new JokeServer(sock).start();
+		}
+		
+	}
+	
+}
